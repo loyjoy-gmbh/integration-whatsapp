fbf007233e4554fa532e2fc4c77ca03056f08a39
Sebastian Deisel <seb.deisel@posteo.de>
2020-08-10 11:46:43 +0200

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,3 +1,18 @@
+export VM_IP_ADDRESS=`curl -fs http://metadata/computeMetadata/v1/instance/attributes/VM_IP_ADDRESS \
+    -H "Metadata-Flavor: Google"`
+
+export WA_DB_HOST_NAME=`curl -fs http://metadata/computeMetadata/v1/instance/attributes/WA_DB_HOST_NAME \
+    -H "Metadata-Flavor: Google"`
+
+export WA_DB_USERNAME=`curl -fs http://metadata/computeMetadata/v1/instance/attributes/WA_DB_USERNAME \
+    -H "Metadata-Flavor: Google"`
+
+export WA_DB_PASSWORD=`curl -fs http://metadata/computeMetadata/v1/instance/attributes/WA_DB_PASSWORD \
+    -H "Metadata-Flavor: Google"`
+
+export WA_API_VERSION=`curl -fs http://metadata/computeMetadata/v1/instance/attributes/WA_API_VERSION \
+    -H "Metadata-Flavor: Google"`
+
 echo "WA_DB_ENGINE=PGSQL" > db.env
 echo "WA_DB_PORT=5432" >> db.env
 echo "WA_DB_HOSTNAME="$WA_DB_HOST_NAME >> db.env
@@ -12,3 +27,4 @@ docker run --rm \
     -v "$PWD:$PWD" \
     -w="$PWD" \
     docker/compose:1.24.0 up -d
+    
