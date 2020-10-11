550411171069a57e2c7a86fbc2c87531e5bf2f5b
Ulrich Wolffgang <uwol@users.noreply.github.com>
2020-10-11 13:51:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ echo "WA_DB_HOSTNAME="$WA_DB_HOST_NAME >> db.env
 echo "WA_DB_USERNAME="$WA_DB_USERNAME >> db.env
 echo "WA_DB_PASSWORD="$WA_DB_PASSWORD >> db.env
 
-sed -i 's/<VM_IP_ADDRESS>/'"$VM_IP_ADDRESS"'/g' install/proxy_config/000-default.conf
-sed -i 's/<WA_API_VERSION>/'"$WA_API_VERSION"'/g' install/docker-compose.yml
+sed -i 's/<VM_IP_ADDRESS>/'"$VM_IP_ADDRESS"'/g' proxy_config/000-default.conf
+sed -i 's/<WA_API_VERSION>/'"$WA_API_VERSION"'/g' docker-compose.yml
 
 docker container prune -f
 docker image prune -f
